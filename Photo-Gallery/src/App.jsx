2e4a109c541a2feb15cfe992ffbed9c7465ce8b9
jfcode101 <125421939+jfcode101@users.noreply.github.com>
2024-10-07 15:44:20 -0400

    initial commit

---
+++ Photo-Gallery/src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+  
+
+  return (
+    <div className="wrapper">
+      <h1>Gallery Application</h1>
+      <p>This is a gallery application</p>
+    </div>
+  )
+}
+
+export default App
